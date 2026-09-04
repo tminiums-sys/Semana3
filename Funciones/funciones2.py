1755d7312130ec85ae3883416b4e80da26d58984
tminiums-sys <tminiums@gmail.com.ni>
2026-09-04 08:41:43 -0600

    Agregando funciones2

--- Funciones/funciones2.py
+++ Funciones/funciones2.py
@@ -1,6 +1,32 @@
-#sumar 2 numeros
 def sumar(num1, num2):
     return num1 + num2
 
 suma = sumar(15, 20)
 print(suma)
+
+
+def restar(num1, num2):
+    return num1 - num2
+
+resta = restar(4, 2)
+print(resta)
+
+resta_con_variables = restar(num1=10, num2=3)
+print(resta_con_variables)
+
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+multiplicacion = multiplicar(4, 2)
+print(multiplicacion)
+
+
+def dividir(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        return "No se puede dividir entre 0"
+
+division = dividir(20, 5)
+print(division)
